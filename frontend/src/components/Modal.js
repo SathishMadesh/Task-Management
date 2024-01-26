bfb4466b27a4630dafc8c3ec385458d2c13465d3
SathishMadesh <s747272946@gmail.com>
2024-01-26 19:51:22 +0530

    update

--- frontend/src/components/Modal.js
+++ frontend/src/components/Modal.js
@@ -5,7 +5,7 @@ class Model extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      activeItem: this.props.activeItem,
+      activeItem: this.props.activeItem, 
     };
   }
 
